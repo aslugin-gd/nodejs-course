cd76b3911917fd333ae4663c4fa18ce013684e9b
Alex Slugin <aslugin@griddynamics.com>
2024-11-21 13:20:02 +0200

    refactor: remove status, error, and stack from error responses

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -6,13 +6,13 @@ const productionError = (err: AppError, res: Response) => {
   // operational error: send message to client about the error
   if (err.isOperational) {
     res.status(err.statusCode).json({
-      status: err.status,
+      statusCode: err.statusCode,
       message: err.message,
     });
   } else {
     // Sends a generic message to the client about the error
     res.status(500).json({
-      status: "error",
+      statusCode: err.statusCode,
       message: "Something went wrong",
     });
   }
@@ -22,16 +22,13 @@ const productionError = (err: AppError, res: Response) => {
 // sends back the error message, and additional information about the error
 const developmentError = (err: AppError, res: Response) => {
   res.status(err.statusCode).json({
-    status: err.status,
+    statusCode: err.statusCode,
     message: err.message,
-    error: err,
-    stack: err.stack,
   });
 };
 
 export const errorHandler = (err: AppError, req: Request, res: Response, next: NextFunction) => {
   err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
 
   if (process.env.NODE_ENV === "development") {
     developmentError(err, res);
