b3b7d4c43984aeb43d012d7647c5111fb9a97254
Alex Slugin <aslugin@griddynamics.com>
2024-11-21 14:43:18 +0200

    fix: sorting by date

--- src/models/exercises.ts
+++ src/models/exercises.ts
@@ -34,7 +34,9 @@ export const getExercisesHandlers = (prisma: PrismaClient) => ({
           gte: from
         }
       },
-
+      orderBy: {
+        date: "asc"
+      }
     });
 
     const count = await prisma.exercise.count({
